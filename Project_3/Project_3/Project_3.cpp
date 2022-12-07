a05e744d331f87f6a6872aeff4077aea4758541e
NicholasOrth <80357606+NicholasOrth@users.noreply.github.com>
2022-12-07 00:58:19 -0500

    Update Project_3.cpp

--- Project_3/Project_3/Project_3.cpp
+++ Project_3/Project_3/Project_3.cpp
@@ -2,65 +2,61 @@
 #include <string>
 #include <vector>
 #include <set>
-#include "Bridges.h"
-#include "DataSource.h"
-#include "data_src/ActorMovieIMDB.h"
 #include <string>
-#include "AVLTreeElement.h"
 #include <map>
+#include <unordered_map>
+#include <fstream>
+#include <algorithm>
+
 using namespace std;
-using namespace bridges;
 
-class Heap {
-    map<string, float> similarList;
+class MovieIMDB {
+    string title;
+    vector<string> genres;
+    int year;
+    bool adult; //if a movie is rated R(?)
 
+public:
+    MovieIMDB() {
+        title = "default";
+        genres.push_back("");
+        year = 0;
+        adult = false;
+    }
+    MovieIMDB(string _title, vector<string> _genres, int _year, bool _adult) {
+        title = _title;
+        genres = _genres;
+        year = _year;
+        adult = _adult;
+    }
+    string getMovie() {
+        return title;
+    }
+    vector<string> getGenres() {
+        return genres;
+    }
+    int getYear() {
+        return year;
+    }
+    bool isAdult() {
+        return adult;
+    }
+    void printMovieInfo() {
+        cout << "Actor-Movie Data:" << endl
+            << "\tMovie: " << title << endl;
+        cout << "\tGenres: ";
+        for (int i = 0; i < genres.size(); i++) {
+            cout << genres[i];
+            if (i != genres.size() - 1)
+                cout << ", ";
+        }
+        cout << endl;
 
+    }
 };
-/*
-  Step 1: Get the Bridges USER ID and API KEY for your program
-          by creating an account and signing up (You can use any
-          email): https://bridges-cs.herokuapp.com/login
 
-  Step 2: User ID (User name) and Key (API Sha1 Key) can be
-          found under Profile on the top left
-*/
-//prints info of the movie
-void printMovieInfo(ActorMovieIMDB& movie) {
-    cout << "Actor-Movie Data:" << endl
-        << "\tActor: " << movie.getActor() << endl
-        << "\tMovie: " << movie.getMovie() << endl
-        << "\tRating: " << movie.getMovieRating() << endl;
-    cout << "\tGenres: ";
-    for (int i = 0; i < movie.getGenres().size(); i++) {
-        cout << movie.getGenres()[i];
-        if (i != movie.getGenres().size() - 1)
-            cout << ", ";
-    }
-    cout << endl;
-
-}
-//prints heap/most similar movies in decending order
-void printHeap(map<float, string, greater<float>>& similar) {
-    int count = 0;
-    for (auto pair : similar) {
-        cout << "similarity rank: " << pair.first << ", Movie: " << pair.second << endl;
-        count++;
-        if (count > 9)
-            break;
-    }
-
-}
-//gets year from title
-int getYear(ActorMovieIMDB& movie) {
-    string s = "";
-    for (auto i = movie.getMovie().find('(') + 1; i < movie.getMovie().find(')');i++) {
-        s += movie.getMovie()[i];
-
-    }
-    return stoi(s);
-}
 //compares 2 movies and makes similarity rank
-float compareMovies(ActorMovieIMDB& movie1, ActorMovieIMDB& movie2) {
+float compareMovies(MovieIMDB& movie1, MovieIMDB& movie2) {
     /*
       simRank is found by taking the number of matching actors, if its made within 5 years of each other, and the number of matching genres, and dividing by the number of genres (on the movie with the least listed) plus 2 (1 for actor and 1 for year), then multiplying by the rating (to factor in the rating of the movie)
     */
@@ -68,9 +64,7 @@ float compareMovies(ActorMovieIMDB& movie1, ActorMovieIMDB& movie2) {
 
     string s = "";
     int total = 2 + min(movie1.getGenres().size(), movie2.getGenres().size());
-    if (movie1.getActor() == movie2.getActor())
-        simRank++;
-    if (abs(getYear(movie1) - getYear(movie2)) < 5)
+    if (abs(movie1.getYear() - movie2.getYear()) < 10)
         simRank++;
     for (int i = 0; i < movie1.getGenres().size(); i++) {
         for (int j = 0; j < movie2.getGenres().size(); j++) {
@@ -78,45 +72,216 @@ float compareMovies(ActorMovieIMDB& movie1, ActorMovieIMDB& movie2) {
                 simRank++;
         }
     }
-    return (simRank / total) * (float)movie2.getMovieRating();
-}
-//create heap (sorted map) with movies with a top similarity score
-void readHeap(vector<ActorMovieIMDB>& actor_list, map<float, string, greater<float>>& similar, int input) {
-    float simRank;
-    int count = 0;
-    float mean;
-    cout << "Movies similar to " << actor_list[input].getMovie() << endl;
-    for (int i = 0; i < actor_list.size(); i++) {
-        if (i != input) {
-            simRank = compareMovies(actor_list[input], actor_list[i]);
+    if (movie1.isAdult() == movie2.isAdult())
+        simRank++;
 
+    return (simRank / total);
+}
+//creates unordered_map with all movies and their similarity rank to user inputed movie
+void createunordered_map(MovieIMDB& userMovie, MovieIMDB& testMovie, unordered_map<float, MovieIMDB>& theHeap) {
+    float simRank = compareMovies(userMovie, testMovie);
+    theHeap.emplace(simRank, testMovie);
+
+    /* if (theHeap.size() <= 10) {
+
+     }
+     else if (simRank > 0) {
+         int count = 0;
+         for (auto it = theHeap.begin(); it != theHeap.end(); it++) {
+             if (count > 10)
+                 break;
+             if (simRank > it->first) {
+                 theHeap.emplace(simRank, testMovie);
+             }
+         }
+     }*/
+
+
+}
+//reads file and compares to user movie, effecively returns a unordered_map using create unordered_map
+void readFile(string fileName, unordered_map<float, MovieIMDB>& theHeap, MovieIMDB& userInput) {
+    cout << "Reading file..." << endl;
+    ifstream inFile;
+    //cout << "test" << endl;
+    inFile.open(fileName);
+    string line, type, title, year, adult, genre, s; //line variable is used for unwanted info, ie. identifier, end year, length etc
+    bool isAdult;
+    vector<string> genres;
+    getline(inFile, line);
+    line = "";
+
+    while (inFile) {
+        getline(inFile, line, '	');
+        //cout << line << " ";
+        line = "";
+        getline(inFile, type, '	');
+        getline(inFile, title, '	');
+        getline(inFile, line, '	');
+        line = "";
+        getline(inFile, adult, '	');
+        getline(inFile, year, '	');
+        getline(inFile, line, '	');
+        line = "";
+        getline(inFile, line, '	');
+        line = "";
+        getline(inFile, genre);
+        size_t pos = 0;
+        while ((pos = genre.find(",")) != string::npos) {
+            s = genre.substr(0, pos);
+            genres.push_back(s);
+            genre.erase(0, pos + 1);
         }
-        if (simRank > 2) {
-            similar.emplace(simRank, actor_list[i].getMovie());
-            mean += simRank;
-            count++;
+        if (adult == "0")
+            isAdult = false;
+        else
+            isAdult = true;
+
+
+        if (year.size() == 2) {
+            year = "0000";
+        }
+
+        if (type == "movie") {
+            MovieIMDB movie(title, genres, stoi(year), isAdult);
+            createunordered_map(userInput, movie, theHeap);
+            //movies.push_back(movie);
+            /*
+            cout << type << ", " << title << ", " << adult << ", " << year << ", ";
+            for (int i = 0; i < genres.size(); i++) {
+                cout << genres[i] << " ";
+            }
+            cout << endl;
+            */
+        }
+        genres.clear();
+        title = "";
+        type = "";
+        genre = "";
+        year = "";
+        adult = "";
+    }
+}
+//prints the top 10 similar movies (assuming unordered_map is sorted)
+void printHeap(unordered_map<float, MovieIMDB>& similar) {
+    int count = 0;
+    for (auto pair : similar) {
+        cout << "similarity rank: " << pair.first << ", Movie: " << pair.second.getMovie() << endl;
+        count++;
+        if (count > 9)
+            break;
+    }
+}
+//takes user input and returns a movie object
+MovieIMDB userInput() {
+    cout << "Movie recommendations! enter the following information: " << endl;
+    cout << "Release year: ";
+    string year;
+    cin >> year;
+
+    cout << "Rated R (type yes or no): ";
+    string adult;
+    cin >> adult;
+
+    cout << "List the genre(s) in a comma seperated list. i.e. action, horror:" << endl;
+    string genre;
+    cin >> genre;
+
+    bool isAdult;
+    if (adult == "no")
+        isAdult = false;
+    else
+        isAdult = true;
+
+    vector<string> genres;
+    size_t pos = 0;
+    string s;
+    while ((pos = genre.find(",")) != string::npos) {
+        s = genre.substr(0, pos);
+        genres.push_back(s);
+        genre.erase(0, pos + 2);
+    }
+    MovieIMDB userMovie("user input", genres, stoi(year), isAdult);
+    return userMovie;
+}
+
+int partition(vector<float>& vec, int left, int right) {
+    int pivotIndex = left + (right - left) / 2;
+    float pivotValue = vec[pivotIndex];
+    int i = left, j = right;
+    float temp;
+    while (i <= j) {
+        while (vec[i] < pivotValue) {
+            i++;
+        }
+        while (vec[j] > pivotValue) {
+            j--;
+        }
+        if (i <= j) {
+            temp = vec[i];
+            vec[i] = vec[j];
+            vec[j] = temp;
+            i++;
+            j--;
         }
     }
-    cout << "count: " << count << ", mean: " << mean / count << endl;
+    return i;
+}
 
+void quicksort(std::vector<float>& vec, int left, int right) {
+    if (left < right) {
+        int pivotIndex = partition(vec, left, right);
+        quicksort(vec, left, pivotIndex - 1);
+        quicksort(vec, pivotIndex, right);
+    }
+}
+
+void mergesort(std::vector<float>& vec)
+{
+    // If the vector has less than two elements, it is already sorted.
+    if (vec.size() < 2) return;
+
+    // Split the vector in half.
+    std::size_t const half = vec.size() / 2;
+    std::vector<float> left(vec.begin(), vec.begin() + half);
+    std::vector<float> right(vec.begin() + half, vec.end());
+
+    // Sort the two halves recursively.
+    mergesort(left);
+    mergesort(right);
+
+    // Merge the two sorted halves into the original vector.
+    std::merge(left.begin(), left.end(), right.begin(), right.end(), vec.begin());
 }
 
 int main(int argc, char** argv) {
 
-    Bridges bridges(1, "north", "58473698554");
-    bridges.setTitle("Accessing IMDB Data");
+    MovieIMDB userMovie = userInput();
 
-    // create data source object
-    DataSource ds(&bridges);
+    //vector<MovieIMDB> movies;
+    // 
+    //unordered_map auto sorts and acts as a sort of heap
+    unordered_map<float, MovieIMDB> theHeap;
+    //    unordered_map<float, MovieIMDB, greater<float>> theHeap;
 
-    // get the actor movie IMDB data through the BRIDGES API
-    vector<ActorMovieIMDB> actor_list = ds.getActorMovieIMDBData2();
-    map<float, string, greater<float>> similar;
-    int input = 222;
 
-    printMovieInfo(actor_list[input]);
-    readHeap(actor_list, similar, input);
-    printHeap(similar);
+    readFile("data.tsv", theHeap, userMovie);
+
+
+    vector<float> quickSortRanks;
+    vector<float> mergeSortRanks;
+
+    for (auto it = theHeap.begin(); it != theHeap.end(); it++) {
+        quickSortRanks.push_back(it->first);
+        mergeSortRanks.push_back(it->first);
+    }
+    //printHeap(theHeap);
+
+    quicksort(quickSortRanks, 0, quickSortRanks.size() - 1);
+    mergesort(mergeSortRanks);
+
+    for (int i = 1; i < 11; i++) {
+        cout << i << ". " << theHeap[quickSortRanks[quickSortRanks.size() - i]].getMovie() << ": " << quickSortRanks[quickSortRanks.size() - i] << endl;
+    }
 
     return 0;
 }
